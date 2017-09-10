c2e93884586c3518ccfc8508f14095c2d84b7c02
Maciek Abramowski <m.abramowski@wp.pl>
2017-09-10 19:45:18 +0200

    username 1

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 console.log('Starting app..');
 
 const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+
 
 //Up to v7 node
 // fs.appendFile('greetings.txt', 'Hello world!');
 
 //Option one
-fs.appendFile('greetings.txt', 'Hello world!', function (err){
+fs.appendFile('greetings.txt', 'Hello ' + user.username + '!', function (err){
   if (err){
     console.log('Unable to write to file');
   }
