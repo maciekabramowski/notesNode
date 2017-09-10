224abd9cb7eaa5214981cd706357f361302c1a9b
Maciek Abramowski <m.abramowski@wp.pl>
2017-09-11 01:19:09 +0200

    3d party modules - lodash

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ console.log('Starting app..');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result:', notes.add(9,-2));
+// console.log(_.isString(true));
+// console.log(_.isString('Andrew'));
+
+var filteredArray = _.uniq(['Andrew',1,'Andrew',1,2,3,4]);
+console.log(filteredArray);
+
+// console.log('Result:', notes.add(9,-2));
 
 // var user = os.userInfo();
 
