ff12890403dac8467cbab1e7bcf2c078cea07249
Maciek Abramowski <m.abramowski@wp.pl>
2017-09-11 02:09:58 +0200

    input from user p.t.1

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-console.log('Starting app..');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+
+console.log('Command:', command);
+console.log(process.argv);
+
+if (command === 'add'){
+  console.log('Adding new note');
+}else if(command==='list'){
+  console.log('Listing all notes');
+}else if(command==='read'){
+  console.log('Fetching last note');
+}else if(command==='remove'){
+  console.log('Removing the note');
+}else{
+  console.log('Command not recognized');
+}
